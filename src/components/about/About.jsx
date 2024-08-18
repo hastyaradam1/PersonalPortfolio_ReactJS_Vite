a0ba13cf6e09ac1a8ea1f90e1652fac4df7c2707
hastyaradam <hasteyarblbas@gmail.com>
2024-08-18 14:59:46 +0300

    Change FullStack to FullStack Web Developer

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 pb-20 md:px-14 lg:px-48">
+        <div className="grid grid-cols-1 xl:grid-cols-3 pb-20 md:px-14 lg:px-48">
           <div
             className="flex justify-center items-center"
             data-aos="zoom-in"
@@ -29,15 +29,15 @@ const About = () => {
           </div>
 
           <div
-            className="col-span-1 xl:col-span-2 space-y-6 px-6"
+            className="col-span-1 md:col-span-1 xl:col-span-2 space-y-6 px-6"
             data-aos="fade-down"
             data-aos-delay="20"
             data-aos-duration="1000"
           >
-            <h1 className="text-2xl md:text-3xl font-semibold">
+            <h1 className="text-2xl font-semibold">
               I'm "{"Hastyar Adam Murad"}",{" "}
               <span className="animate-opacity-change bg-gradient-to-r font-bold from-red-200 to-blue-400 bg-clip-text text-transparent">
-                Full Stack Developer...
+                Full Stack Web Developer...
               </span>
             </h1>
             <p className="text-justify text-sm md:text-lg text-zinc-300">
